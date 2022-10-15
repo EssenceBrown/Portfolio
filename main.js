44b48196f829d5e63b5d47356cb71e5e67732235
EssenceBrown <essence@cccareers.org>
2022-10-15 16:37:16 -0700

    icons and email input added

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 console.log("hello world");
 
 alert('test')
+
+
+
+
+// let tablinks = document.getElementsByClassName("tab-links");
+// let tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname){
+//     for(tablink of tablinks){
+//         tablink.classlist.remove("active-link");
+//     }
+//     for(tabcontent of tabcontents){
+//         tabcontent.classlist.remove("active-tab");
+//     }
+// }
