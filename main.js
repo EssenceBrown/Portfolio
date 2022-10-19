335236a6c52cecddb8e8c5c7381361fd787609e6
EssenceBrown <essence@cccareers.org>
2022-10-18 23:27:59 -0400

    added a contact page

--- main.js
+++ main.js
@@ -22,3 +22,22 @@ function showContents(activeTab, contentsId){
     document.getElementById(contentsId).classList.add('show');
 }
 
+// this section is for the contact information 
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
